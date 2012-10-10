7cd34be1a63dae2f1e13d47da8b59ffb51b5880d
Steven Luu <sluu99@gmail.com>
2012-10-10 02:38:30 -0400

    fixed javascript

--- static/index.js
+++ static/index.js
@@ -7,21 +7,31 @@ $(function() {
 	urlBox.focus();
 });
 
+function err(str) {
+	msgDiv.text(str);
+}
+
 function shawty(data) {
 	if (data.success == 0) {
 		msgDiv.text(data.message)
 	}
 	else {
-		msgDiv.text(' ')
+		err(' ');
 		urlBox.val(data.short);
-		urlBox.focus();
-		urlBox.select();
-	}			
+	}
+
+	urlBox.focus();
+	urlBox.select();
 }
 
 function shorten() {
 	var urlStr = urlBox.val();
-	$.getScript('shawty.js?url=' + escape(urlStr));
+	urlStr = $.trim(urlStr);
+	urlBox.val(urlStr);
+	if (urlStr != '')
+		$.getScript('shawty.js?url=' + escape(urlStr));
+	else 
+		err('a valid url has to start with http:// or https://');
 
 	return false;
 }
